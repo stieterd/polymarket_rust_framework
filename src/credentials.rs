feef5ac597dd3241b2bbc2f03d8cc639ed7d0be0
stieterd <peviho@outlook.com>
2025-11-01 17:01:37 +0100

    latest edits

--- src/credentials.rs
+++ src/credentials.rs
@@ -2,13 +2,13 @@ use ethers::types::H160;
 use lazy_static::lazy_static;
 use std::str::FromStr;
 
-pub const POLY_API_KEY: &str = "adc2800b-aebf-eff9-e3f5-b93a040a88c1";
-pub const POLY_API_SECRET: &str = "t1rrbJOvLjMFQuFJ61po_Z8Rfk5WhhHsbtzQaZuqHZg=";
+pub const POLY_API_KEY: &str = "db79e748-95ee-36cd-f9ab-00143377acba";
+pub const POLY_API_SECRET: &str = "Mcmj_edaTf_mxsPKLOu8yRelIduEbo3BP1W4izj85js=";
 pub const POLY_API_PASSPHRASE: &str =
-    "c9ca4780654a8851c8355c5b49531a0a2279291dca18283f6db91e4e1bd31184";
+    "c2f920da38ada61ae1ad55f9ee4bfe084fc6f6ade06623488f232a3529940e0f";
 
-pub const PRIVATE_KEY: &str = "0x45db5ff8b6898ac5e4119b88ff189a7f3318a518685a822d038f12a6abe2b8da";
-pub const ADDRESS_STR: &str = "0xb48b9192DC52eED724Fa58c66Fa8926d06A3648e";
+pub const PRIVATE_KEY: &str = "0x8dc78334ff702005b631e249d1e02e76e179af634e4c3869add8dc007b4de411";
+pub const ADDRESS_STR: &str = "0xB0A60787710f8D6254dC0E304Fc72b6A3907e0C2";
 lazy_static! {
     pub static ref ADDRESS: H160 = H160::from_str(ADDRESS_STR).unwrap();
 }
